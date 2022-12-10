8bf0fe58776c390b34f3f711aa8078211d817388
Emirhan Erdoğdu <emirhan.erdogdu@std.yildiz.edu.tr>
2022-12-10 19:44:04 +0300

    ..

--- Data.2.py
+++ Data.2.py
@@ -1,3 +1,6 @@
+f = open("data","w")
+f.write("Name: Emirhan\nSurname: Erdogdu\nGender: Sek erkek\nUsername: Crazyboy_31\nJob: Playboy")
+f.close()
 f = open("data", "r")
 curt = f.read().split("\n")
 dict = {}
